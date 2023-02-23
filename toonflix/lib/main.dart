0076ae4312b6b7d9840888d204acb07f1479f152
hyungraelee <hyunlee.dev@gmail.com>
2023-02-23 21:11:44 +0900

    3.5 Reusable Widgets(nomadcoders)

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -1,8 +1,8 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:toonflix/widgets/button.dart';
 
 void main() {
-  runApp(App());
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
@@ -15,7 +15,7 @@ class App extends StatelessWidget {
           backgroundColor: const Color(0XFF181818),
           body: Padding(
             padding: const EdgeInsets.symmetric(
-              horizontal: 40,
+              horizontal: 20,
             ),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -73,27 +73,20 @@ class App extends StatelessWidget {
                   height: 30,
                 ),
                 Row(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Colors.amber,
-                        borderRadius: BorderRadius.circular(45),
-                      ),
-                      child: const Padding(
-                        padding: EdgeInsets.symmetric(
-                          vertical: 20,
-                          horizontal: 50,
-                        ),
-                        child: Text(
-                          'Transfer',
-                          style: TextStyle(
-                            fontSize: 20,
-                          ),
-                        ),
-                      ),
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Button(
+                      text: 'Transfer',
+                      bgColor: Color(0xFFF1B33B),
+                      textColor: Colors.black,
+                    ),
+                    Button(
+                      text: 'Request',
+                      bgColor: Color(0XFF1F2123),
+                      textColor: Colors.white,
                     ),
                   ],
-                )
+                ),
               ],
             ),
           )),
