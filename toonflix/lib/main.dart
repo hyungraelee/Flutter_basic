d5460aceb7ff9763ab34815cf633d4ec0f8b8c4c
hyungraelee <hyunlee.dev@gmail.com>
2023-03-02 20:36:48 +0900

    6.6 FutureBuilder(nomadcoders)

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:toonflix/screens/home_screen.dart';
-import 'package:toonflix/services/api_service.dart';
 
 void main() {
   runApp(const App());
@@ -13,9 +12,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Container(
-        child: const HomeScreen(),
-      ),
+      home: HomeScreen(),
     );
   }
 }
